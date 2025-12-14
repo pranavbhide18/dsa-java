2751a10fa83d8d8b0d8e38085763cd15bb89b4b8
pranavbhide18 <pranav.bhide18@gmail.com>
2025-12-14 10:45:58 +0530

    solve stock buy-sell

---
+++ src/Arrays/StockBuySell.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class StockBuySell {
+    void main() {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            int cost = prices[i] - min;
+            profit = Math.max(profit, cost);
+
+            min = Math.min(min, prices[i]);
+        }
+
+        return profit;
+
+    }
+
+}
